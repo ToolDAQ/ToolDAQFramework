9ba048b24dfb236aea380bafb4541ea148c045d4
Benjamin richards <b.richards@qmul.ac.uk>
2016-07-15 18:50:48 +0100

    travis update

--- CI.sh
+++ CI.sh
@@ -1,8 +1,51 @@
 #!/bin/bash
 
-./GetToolDAQ.sh
+mkdir ToolDAQ
+cd ToolDAQ
 
-source Setup.sh
+git clone https://github.com/ToolDAQ/ToolDAQFramework.git
+git clone https://github.com/ToolDAQ/zeromq-4.0.7.git
+wget http://downloads.sourceforge.net/project/boost/boost/1.60.0/boost_1_60_0.tar.gz
+tar zxf boost_1_60_0.tar.gz
+
+cd zeromq-4.0.7
+
+./configure --prefix=`pwd`
+make
+make install
+
+export LD_LIBRARY_PATH=`pwd`/lib:$LD_LIBRARY_PATH
+
+cd ../boost_1_60_0
+
+mkdir install
+
+./bootstrap.sh --prefix=`pwd`/install/ > /dev/null 2>/dev/null
+./b2 install
+
+export LD_LIBRARY_PATH=`pwd`/install/lib:$LD_LIBRARY_PATH
+
+cd ../ToolDAQFramework
+
+make clean
+make
+
+export LD_LIBRARY_PATH=`pwd`/lib:$LD_LIBRARY_PATH
+
+cd ..
+
+get https://root.cern.ch/download/root_v5.34.34.source.tar.gz
+tar zxf root_v5.34.34.source.tar.gz
+cd root
+
+./configure --prefix=`pwd` --enable-rpath > /dev/null 2>/dev/null
+make
+make install
+
+
+cd ../../
+
+make clean
+make
 
 ./main configfiles/ToolChainConfig
-
