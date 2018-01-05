3518ef9b970b0ebcb09e55549646c1bb7e4c2f5c
brichards64 <b.richards@qmul.ac.uk.com>
2018-01-05 16:34:07 +0000

    edited Serialisable object

--- src/Store/SerialisableObject.h
+++ src/Store/SerialisableObject.h
@@ -1,14 +1,21 @@
 #ifndef SERIALISABLEOBJECT_H
 #define SERIALISABLEOBJECT_H
 
-#include <string>
+#include <boost/archive/text_oarchive.hpp>
+#include <boost/archive/text_iarchive.hpp>
+#include <boost/archive/binary_oarchive.hpp>
+#include <boost/archive/binary_iarchive.hpp>
+#include <boost/serialization/vector.hpp>
+#include <boost/serialization/map.hpp>
 
 class SerialisableObject{
-
+  
+  friend class boost::serialization::access;
+  
  public:
-
+  
   virtual bool Print()=0;
-  //  virtual ~SerialisableObject();
+  virtual ~SerialisableObject(){};
   
  protected:
   
@@ -16,12 +23,18 @@ class SerialisableObject{
   bool serialise;
   std::string version;
   
+  template<class Archive> void serialize(Archive & ar, const unsigned int version){  
+    if(serialise){
+      ar & type;
+      ar & version;
+    }
+  }
+  
+  
  private:
-  
-  
-
-
 
 };
+
+
 
 #endif
