787f881e2e74f80ca63193eb9c35922b154e0c6b
brichards <benjamin.richards@warwick.ac.uk>
2021-06-11 11:29:30 +0100

    added group control command

--- src/RemoteControl/RemoteControl.cpp
+++ src/RemoteControl/RemoteControl.cpp
@@ -15,6 +15,7 @@
 #define MULTICAST_PORT 5000
 #define KICK_TIME_SEC 30
 #define COMMAND_REPLY_WAIT 120000
+#define GROUP_COMMAND_REPLY_WAIT 2000
 #define FILE_SEND_WAIT 120000
 #define FILE_SEND_PORT 24001
 
@@ -129,38 +130,38 @@ int main(int argc, char** argv){
 
 
     else if(input.str()=="Quit")running=false;
-  
-    else{
+    std::string Command;
+    input>>Command;  
 
-      std::string Command;
+    else if{Command=="Command"){
+
       int ServiceNum;
-
+      
       std::string Send;
       std::string var1;
-
-      input>>Command>>ServiceNum>>Send>>var1;
-
-      if(Command=="Command"){
-
-	if(ServiceNum<RemoteServices.size()){
-
+      
+      input>>ServiceNum>>Send>>var1;
+      
+      
+      if(ServiceNum<RemoteServices.size()){
+	
 	zmq::socket_t ServiceSend (context, ZMQ_REQ);
 	int a=COMMAND_REPLY_WAIT;
 	ServiceSend.setsockopt(ZMQ_RCVTIMEO, a);
 	ServiceSend.setsockopt(ZMQ_SNDTIMEO, a);
-
+	
 	std::stringstream connection;
 	connection<<"tcp://"<<*((*(RemoteServices.at(ServiceNum)))["ip"])<<":"<<*((*(RemoteServices.at(ServiceNum)))["remote_port"]);
 	ServiceSend.connect(connection.str().c_str());
-
-
+	
+	
 	boost::posix_time::ptime t = boost::posix_time::microsec_clock::universal_time();
 	std::stringstream isot;
 	isot<<boost::posix_time::to_iso_extended_string(t) << "Z";
-
+	
 	msg_id++;
 	Store bb;
-
+	
 	bb.Set("uuid",m_UUID);
 	bb.Set("msg_id",msg_id);
 	*bb["msg_time"]=isot.str();
@@ -180,11 +181,11 @@ int main(int argc, char** argv){
 	
 	/////////////////////////////////
 	if(Send=="File"){
-
+	  
 	  std::stringstream connection;
 	  connection<<"tcp://"<<*((*(RemoteServices.at(ServiceNum)))["ip"])<<":"<<FILE_SEND_PORT;
-
-	   
+	  
+	  
 	  zmq::socket_t ftp (context, ZMQ_PUSH);
 	  int a=FILE_SEND_WAIT;
 	  ftp.setsockopt(ZMQ_RCVTIMEO, a);
@@ -222,8 +223,8 @@ int main(int argc, char** argv){
 	  
 	  ///////
 	}
-
-
+	
+	
         zmq::message_t receive;
         if(ServiceSend.recv(&receive)){
 	  std::istringstream iss(static_cast<char*>(receive.data()));
@@ -236,18 +237,147 @@ int main(int argc, char** argv){
 	  if(*rr["msg_type"]=="Command Reply") std::cout<<std::endl<<*rr["msg_value"]<<std::endl<<std::endl;
 	}
 	else std::cout<<std::endl<<"message timed out"<<std::endl; 
-	  
-	}
 	
-	else std::cout<< "Service number out of range"<<std::endl<<std::endl;
       }
-
-
-    else std::cout<<"Error not a valid command"<<std::endl<<std::endl;
+      
+      else std::cout<< "Service number out of range"<<std::endl<<std::endl;
+     
 
     }
+
+
+
+
+
+
+
+
+
+
+
+    //////////////
+    else if(Command=="Group"){
+
+      std::string ServiceName;
+      
+      std::string Send;
+      std::string var1;
+      
+      input>>ServiceName>>Send>>var1;
+      
+      for(int i=0; i<RemoteServices.size(); i++){
+	
+	std::string service;
+	service=*((*(RemoteServices.at(i)))["msg_value"]);
+	
+	if(service==ServiceName){
+	  
+	  zmq::socket_t ServiceSend (context, ZMQ_REQ);
+	  int a=GROUP_COMMAND_REPLY_WAIT;
+	  ServiceSend.setsockopt(ZMQ_RCVTIMEO, a);
+	  ServiceSend.setsockopt(ZMQ_SNDTIMEO, a);
+	  
+	  std::stringstream connection;
+	  connection<<"tcp://"<<*((*(RemoteServices.at(i)))["ip"])<<":"<<*((*(RemoteServices.at(i)))["remote_port"]);
+	  ServiceSend.connect(connection.str().c_str());
+	  
+	  
+	  boost::posix_time::ptime t = boost::posix_time::microsec_clock::universal_time();
+	  std::stringstream isot;
+	  isot<<boost::posix_time::to_iso_extended_string(t) << "Z";
+	  
+	  msg_id++;
+	  Store bb;
+	  
+	  bb.Set("uuid",m_UUID);
+	  bb.Set("msg_id",msg_id);
+	  *bb["msg_time"]=isot.str();
+	  *bb["msg_type"]="Command";
+	  bb.Set("msg_value",Send);
+	  bb.Set("var1",var1);
+	  
+	  
+	  std::string tmp="";
+	  bb>>tmp;
+	  zmq::message_t send(tmp.length()+1);
+	  snprintf ((char *) send.data(), tmp.length()+1 , "%s" ,tmp.c_str()) ;
+	  
+	  ServiceSend.send(send);
+	  
+	  
+	  
+	  /////////////////////////////////
+	  if(Send=="File"){
+	    
+	    std::stringstream connection;
+	    connection<<"tcp://"<<*((*(RemoteServices.at(i)))["ip"])<<":"<<FILE_SEND_PORT;
+	    
+	    
+	    zmq::socket_t ftp (context, ZMQ_PUSH);
+	    int a=FILE_SEND_WAIT;
+	    ftp.setsockopt(ZMQ_RCVTIMEO, a);
+	    ftp.setsockopt(ZMQ_SNDTIMEO, a);
+	    ftp.connect(connection.str().c_str());
+	    
+	    // zmq::message_t file(256);
+	    std::string line;
+	    std::ifstream myfile (var1.c_str());
+	    if (myfile.is_open())
+	      {
+		while ( getline (myfile,line) )
+		  {
+		    //line+="\n";
+		  zmq::message_t file(line.length()+1);
+		  //std::cout<<"debug = "<<line.c_str()<<std::endl;
+		  // memcpy(file.data(), line.c_str(), line.length());
+		  snprintf ((char *) file.data(), line.length()+1 , "%s" ,line.c_str()) ;
+		  //std::cout<<"sending part"<<std::endl;
+		  if(!ftp.send(file,ZMQ_SNDMORE))break;
+		  //ftp.send(file);
+		  //std::cout<<"sendt part ="<<line.c_str()<<std::endl;
+		  }
+		myfile.close();
+	      }
+	    
+	    zmq::message_t end(2);
+	    std::string tmp2="";
+	    snprintf ((char *) end.data(), 2 , "%s" ,tmp2.c_str()) ;
+	    //std::cout<<"sending end"<<std::endl;
+	    ftp.send(end,0);
+	    //std::cout<<"sent end"<<std::endl;
+	    
+	    
+	    
+	    ///////
+	  }
+	  
+	  
+	  zmq::message_t receive;
+	  if(ServiceSend.recv(&receive)){
+	    std::istringstream iss(static_cast<char*>(receive.data()));
+	    
+	    std::string answer;
+	    answer=iss.str();
+	    
+	    Store rr;
+	    rr.JsonParser(answer);
+	    if(*rr["msg_type"]=="Command Reply") std::cout<<std::endl<<*rr["msg_value"]<<std::endl<<std::endl;
+	  }
+	}
+      }
+      
+      
+      
+      
+      
+    }
+    //////////////////
+    
+    else std::cout<<"Error not a valid command"<<std::endl<<std::endl;
+
   }
-
+  
+  
   return 0;
-
+  
 }
