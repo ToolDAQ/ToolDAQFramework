b0d613c3c3840e12277b03b9fd14534ded3c56d6
brichards <benjamin.richards@warwick.ac.uk>
2022-01-23 21:10:59 +0000

    corrected typo in setup.sh

--- Application/Setup.sh
+++ Application/Setup.sh
@@ -6,6 +6,6 @@ Dependencies=`pwd`
 
 #source ${ToolDAQapp}/ToolDAQ/root/bin/thisroot.sh
 
-export LD_LIBRARY_PATH=`pwd`/lib:${Dependencies)/zeromq-4.0.7/lib:${Dependencies}/boost_1_66_0/install/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=`pwd`/lib:${Dependencies}/zeromq-4.0.7/lib:${Dependencies}/boost_1_66_0/install/lib:$LD_LIBRARY_PATH
 
 export SEGFAULT_SIGNALS="all"
