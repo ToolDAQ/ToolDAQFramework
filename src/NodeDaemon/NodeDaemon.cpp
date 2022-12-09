4b0614c64e4256f2be3cf0bc5630210477b1b910
brichards64 <b.richards@qmul.ac.uk>
2022-12-09 05:31:16 -0600

    added acidentally removed node daemon name from service discovery

--- src/NodeDaemon/NodeDaemon.cpp
+++ src/NodeDaemon/NodeDaemon.cpp
@@ -98,7 +98,7 @@ int main(int argc, char* argv[]){
 
   std::stringstream service;
   if(argc ==2) service<<argv[1];
-  else{service<<"Node Daemon";}
+  service<<" Node Daemon";}
 
   ServiceDiscovery SD(true, false, REMOTE_PORT, MULTICAST_ADDRESS, MULTICAST_PORT, &context, m_UUID, service.str());
 
