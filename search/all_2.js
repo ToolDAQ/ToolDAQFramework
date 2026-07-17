40cd91bebe42d4cba05ad4b071d5cc83b2e71474
brichards64 <brichards64@users.noreply.github.com>
2026-07-17 10:09:35 +0000

    deploy: b9601e3ca79eb4a432d8c3b57e359e18703b0ee8

--- search/all_2.js
+++ search/all_2.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['addport_4',['AddPort',['../classToolFramework_1_1DAQUtilities.html#a91596b17a158f8a2a16ae906aea43ef0',1,'ToolFramework::DAQUtilities']]],
-  ['addservice_5',['AddService',['../classToolFramework_1_1DAQUtilities.html#adc1db69680c293e8e83c832371d42f4c',1,'ToolFramework::DAQUtilities']]],
+  ['addservice_5',['AddService',['../classToolFramework_1_1DAQUtilities.html#adc1db69680c293e8e83c832371d42f4c',1,'ToolFramework::DAQUtilities::AddService(std::string ServiceName, unsigned int port, bool StatusQuery=false)'],['../classToolFramework_1_1DAQUtilities.html#a53d355419cf6e222f4ef6ed8f83343ee',1,'ToolFramework::DAQUtilities::AddService(std::vector&lt; std::string &gt; ServiceName, std::vector&lt; unsigned int &gt; port, std::vector&lt; bool &gt; StatusQuery)']]],
   ['alarmmsg_6',['AlarmMsg',['../structToolFramework_1_1AlarmMsg.html',1,'ToolFramework']]],
   ['algo_5ftime_5ft_7',['algo_time_t',['../structalgo__time__t.html',1,'']]]
 ];
