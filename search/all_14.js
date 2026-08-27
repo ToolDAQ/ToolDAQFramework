54c560d6b5a94d8151915378307e01fce4398ecc
brichards64 <brichards64@users.noreply.github.com>
2026-08-27 16:09:37 +0000

    deploy: 723543f13a5d236567b0f1f655fa8270b6ce9e86

--- search/all_14.js
+++ search/all_14.js
@@ -9,6 +9,6 @@ var searchData=
   ['total_5flen_294',['total_len',['../structXXH64__state__s.html#af9e2c07ff283799907491063fbde80d5',1,'XXH64_state_s']]],
   ['total_5flen_5f32_295',['total_len_32',['../structXXH32__state__s.html#a483391d07919dedaa835f67672328c61',1,'XXH32_state_s']]],
   ['tuning_20parameters_296',['Tuning parameters',['../group__tuning.html',1,'']]],
-  ['type_297',['Type',['../classToolFramework_1_1BoostStore.html#a50fb58e713af0920daed1be64fd7e23c',1,'ToolFramework::BoostStore']]],
-  ['type_298',['type',['../classToolFramework_1_1BSerialisableObject.html#ac1c3edd941971c9d4f87bcb98a210777',1,'ToolFramework::BSerialisableObject']]]
+  ['type_297',['type',['../classToolFramework_1_1BSerialisableObject.html#ac1c3edd941971c9d4f87bcb98a210777',1,'ToolFramework::BSerialisableObject']]],
+  ['type_298',['Type',['../classToolFramework_1_1BoostStore.html#a50fb58e713af0920daed1be64fd7e23c',1,'ToolFramework::BoostStore']]]
 ];
